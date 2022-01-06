e3d99903c71ffd544c2ac2f39dd21f9923574548
Bolter87 <workw6132@gmail.com>
2022-01-06 16:13:03 +0100

    product api

---
+++ back-end/spring-boot-ecommerce/src/main/java/com/onlineshop/springbootecommerce/config/DataRestConfig.java
@@ -0,0 +1,31 @@
+package com.onlineshop.springbootecommerce.config;
+
+import com.onlineshop.springbootecommerce.entity.Product;
+import com.onlineshop.springbootecommerce.entity.Category;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+/**
+ * Disable put, post, delete methode for product & category
+ */
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedAction = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
+
+        config.getExposureConfiguration().forDomainType(Product.class)
+                .withItemExposure((metadata, HttpMethods) -> HttpMethods.disable(theUnsupportedAction))
+                .withCollectionExposure((metadata, HttpMethods) -> HttpMethods.disable(theUnsupportedAction));
+
+        config.getExposureConfiguration().forDomainType(Category.class)
+                .withItemExposure((metadata, HttpMethods) -> HttpMethods.disable(theUnsupportedAction))
+                .withCollectionExposure((metadata, HttpMethods) -> HttpMethods.disable(theUnsupportedAction));
+
+    }
+
+}
