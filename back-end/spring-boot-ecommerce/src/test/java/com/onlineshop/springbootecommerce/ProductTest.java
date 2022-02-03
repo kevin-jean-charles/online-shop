8ccfce3245fb2b651362cdcfe4b2c30ddef55f3d
Bolter87 <workw6132@gmail.com>
2022-02-03 11:45:19 +0100

    Doing : add product test

--- back-end/spring-boot-ecommerce/src/test/java/com/onlineshop/springbootecommerce/ProductTest.java
+++ back-end/spring-boot-ecommerce/src/test/java/com/onlineshop/springbootecommerce/ProductTest.java
@@ -2,37 +2,37 @@ package com.onlineshop.springbootecommerce;
 
 import com.onlineshop.springbootecommerce.entity.Product;
 
-import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 
 public class ProductTest {
-    
-    //declerer un produit
+
+    // declerer un produit
     private Product productTest;
+    // private 
 
-    //setup product test
 
-    @Before
+    // setup product test
+    @BeforeAll
     public void setup() {
         this.productTest = new Product();
-        this.productTest.setId(1);
-  
-    //   this.testUser = new User();
-    //   this.testUser.setName("Hermione Granger");
-    //   this.testUser.setEmail(email);
-    //   this.testUser.setHashedpw("somehashedpw");
-    //   this.jwt = "somemagicjwt";
-    //   mongoClient
-    //       .getDatabase(databaseName)
-    //       .getCollection("users")
-    //       .deleteOne(new Document("email", "log@out.com"));
+        this.productTest.setId(1L);
+        this.productTest.setActive(true);
+        this.productTest.setBarcode("barcode");
+        // this.productTest.setCategory(category);
+
+        // this.testUser = new User();
+        // this.testUser.setName("Hermione Granger");
+        // this.testUser.setEmail(email);
+        // this.testUser.setHashedpw("somehashedpw");
+        // this.jwt = "somemagicjwt";
+        // mongoClient
+        // .getDatabase(databaseName)
+        // .getCollection("users")
+        // .deleteOne(new Document("email", "log@out.com"));
     }
-
-
 
     // tester creation de produit
 
-
-    
     // tester recherche de un produit
     // tester recherche de tout les produits
     // tester recherche de catégorie du produit
@@ -40,5 +40,4 @@ public class ProductTest {
     // tester modification du produit
     // tester modification de la categoriy du produit
     // tester suppression de produit
-}
 }
